6e3a34d34ec1a6f7520adcc09fbbad278c337ce2
siamalrafi <rahathasandipto@gmail.com>
2023-07-23 13:37:16 +0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import RootLayout from "@/components/Layouts/RootLayout";
+import Banner from "@/components/UI/Banner";
+import AllNews from "@/components/UI/AllNews";
+
+const HomePage = ({ allNews }) => {
+   console.log(allNews);
+   return (
+      <>
+         <Head>
+            <title>PH-News Portal</title>
+            <meta name="description" content="This is news portal of programming hero made by next-js" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+         <Banner />
+         <AllNews allNews={allNews} />
+      </>
+   );
+};
+export default HomePage;
+
+HomePage.getLayout = function getLayout(page) {
+   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+   const res = await fetch("http://localhost:5000/news");
+   const data = await res.json();
+   return { props: { allNews: data }, revalidate: 10 };
+};
